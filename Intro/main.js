308162c502b59e9e0b8cca6e5d917a2e7d4ca671
DesislavaF <156531205+DesislavaF@users.noreply.github.com>
2024-10-06 10:16:15 +0300

    Intro TypeScript

---
+++ Intro/main.js
@@ -0,0 +1,6 @@
+var isAuth = true;
+console.log("Pesho is Authenticated: ".concat(isAuth));
+var arr = ["a", "b", "c"];
+arr.forEach(function (letter) {
+    console.log(letter);
+});
---
+++ Intro/main.ts
@@ -0,0 +1,8 @@
+let isAuth: boolean = true;
+console.log(`Pesho is Authenticated: ${isAuth}`);
+
+const arr: string[] = ["a", "b", "c"];
+
+arr.forEach((letter) => {
+    console.log(letter);
+});
